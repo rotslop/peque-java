29b9f4c640296556a018d10e42bfe08a94d9bb3e
qmarqeva <qmarqeva@0ed0a6c0-136b-11df-9b47-755de4fef228>
2010-02-07 18:18:48 +0000

    - Implementada la parte de Carga de la Tabla - Implementada la Parte de Remplazo de los Tokens por símbolos
    * Hay errores en el Analizador Lexico
    * Problemas al detectar el Segundo ejercicio

--- trunk/PequeJavañol/src/clases/Sintactico.java
+++ trunk/PequeJavañol/src/clases/Sintactico.java
@@ -21,12 +21,15 @@
  * - El analizador léxico no reconce todos los ; en la salida
  * - Ignora la parte de VARIABLES INICIO FIN en la salida
  *      Esto hace que el léxico de errores luego.... ojo
+ * - Se necesita que la salida del analizador separe los == <= >= <>
+ *   como símbolos a parte.
  */
 package clases;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -37,6 +40,31 @@ public class Sintactico {
 
     //Tabla de Coincidencias Analisis LL(1)
     private static String[][] tabla = new String[31][37];
+    //Pila de Analisis Sintáctico
+    private static ArrayList<String> pila = new ArrayList<String>();
+    //Entrada de Prueba
+    //private static String[] datos = {"VARIABLES", "VAR", "ID", "BOOL", ";",
+    //    "INICIO", "LEER", "(", "ID", ")", ";", "FIN", "$", ""};
+    private static String[] datos = {"VARIABLES", "VAR", "ID", "ENTERO", ";", "VAR",
+        "ID", "CADENA", ";", "INICIO", "REPETIR", "(",
+        "ID", "=", "=", "NUM", ")", "{", "ESCRIBIR", "(",
+        "NUM", ")", ";", "}", "SI", "(", "TRUE", ")",
+        "{", "}", "SINO", "{", "ID", "=", "NUM", "+",
+        "NUM", ";", "}", "FIN", "$"};
+    static ArrayList<String> intro = new ArrayList<String>();
+
+    /**
+     * Solo para pruebas
+     * Crea un ArrayList similar al que envia el
+     * Analizador Lexico
+     */
+    public static void llenado() {
+        for (int i = 0; i < datos.length; i++) {
+            intro.add(datos[i]);
+        }
+        pila.add("$");
+        pila.add("PROGRAMA");
+    }
 
     /**
      * Cargará la estructura de la tabla desde un archivo.
@@ -92,6 +120,22 @@ public class Sintactico {
                 e2.printStackTrace();
             }
         }
+        System.out.println("TABLA LL(1) ok");
+        corregir();
+    }
+
+    /**
+     * Corregir Tabla
+     */
+    public static void corregir() {
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[0].length; j++) {
+                if (tabla[i][j] == null) {
+                    tabla[i][j] = "ERROR";
+                }
+            }
+        }
+        System.out.println("CORRECION ok");
     }
 
     /**
@@ -133,10 +177,10 @@ public class Sintactico {
         tok.put("INI_LLA", "}");
         tok.put("FIN_LLA", "}");
 
-        if (tok.get(token) == null){
+        if (tok.get(token) == null) {
             return "";
         }
-        return  (String)tok.get(token) ;
+        return (String) tok.get(token);
     }
 
     /**
@@ -144,8 +188,15 @@ public class Sintactico {
      * terminal
      * @param terminal
      * @return posición (columna) o -1 sino existe
+     *
+     * ESTADO : TERMINADO
      */
-    public int buscarElementoTerminal(String terminal) {
+    public static int buscarElementoTerminal(String terminal) {
+        for (int i = 0; i < tabla[0].length; i++) {
+            if (tabla[0][i].equals(terminal)) {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -154,8 +205,15 @@ public class Sintactico {
      * no terminal solicitado
      * @param noterminal
      * @return posición (columna) o -1 sino existe
+     *
+     * ESTADO : TERMINADO
      */
-    public int buscarElementoNoTerminal(String noterminal) {
+    public static int buscarElementoNoTerminal(String noterminal) {
+        for (int i = 0; i < tabla.length; i++) {
+            if (tabla[i][0].equals(noterminal)) {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -166,22 +224,21 @@ public class Sintactico {
      * @param col
      * @return coincidencia de la tabla
      */
-    public String extraerConcidencia(int fil, int col) {
-        return "";
+    public static String extraerConcidencia(int fil, int col) {
+        return tabla[fil][col];
     }
 
     /**
-     * Elimina el elemento del inicio del arreglo cuando existe una coincidencia
-     * en la Entrada
+     * Elimina el elemento del fin y de inicio de
+     * la pila y de la entrada respectivamente.
+     * ESTADO : COMPLETO
      */
-    public void elimCndEntrada() {
-    }
-
-    /**
-     * Elimina el elemento del final del arreglo de Analisis
-     * al Encontrar una coincidencia
-     */
-    public void elimCndAnalisis() {
+    public static void coincidencia() {
+        System.out.println("COINCIDENCIA [" + pila.get(pila.size() - 1) + "]==[" + intro.get(0) + "]");
+        //Elimina el último de la pila
+        pila.remove(pila.get(pila.size() - 1));
+        //Elimina el primero de la entrada
+        intro.remove(0);
     }
 
     /**
@@ -189,15 +246,91 @@ public class Sintactico {
      * y la inserta al final del arreglo de Analisis
      * previa la eliminación del anterior.
      * @param entrada
+     * ESTADO : COMPLETO
      */
-    public void invertir(String entrada) {
+    public static void invertir(String entrada) {
+        String[] dat = entrada.split(" ");
+        //Elimina el último
+        pila.remove(pila.size() - 1);
+        //Lo reemplaza con la coincidencia inversa
+        for (int i = dat.length - 1; i > -1; i--) {
+            pila.add(dat[i]);
+        }
     }
 
+    /**
+     * Presentación Arreglos
+     */
+    public static void presentar() {
+        System.out.println("");
+        for (String a : pila) {
+            System.out.print(a + "  ");
+        }
+        System.out.print("              ");
+        for (String a : intro) {
+            System.out.print(a + "  ");
+        }
+    }
     //AREA DE TESTING
+
     public static void main(String[] args) {
 
         cargarTabla();
-        presentarTabla();
+        //   presentarTabla();
 
+        llenado();
+        System.out.println("--------------");
+        boolean error = false;
+        boolean continuar = true;
+
+        presentar();
+
+        while (continuar && !error) {
+
+            if (pila.size() == 4){
+                System.out.println("");
+            }
+
+            int col = buscarElementoTerminal(intro.get(0));
+            int fil = buscarElementoNoTerminal(pila.get(pila.size() - 1));
+
+            //System.out.println("[" + pila.get(pila.size() - 1) + "][" + intro.get(0) + "]");
+
+            //Localizo el elemento en la tabla
+            String sal = extraerConcidencia(fil, col);
+            if (sal.equals("ERROR")) {
+                System.out.println("ERROR EN LA ENTRADA[" + pila.get(pila.size() - 1) + "][" + intro.get(0) + "]");
+                error = true;
+                continuar = false;
+                break;
+            }
+            //Es vacío, elimino el último elemento de la pila.
+            if (sal.toUpperCase().equals("E")) {
+                pila.remove(pila.get(pila.size() - 1));
+            } else {
+                //convierto la coincidencia a la inversa en la pila
+                invertir(sal);
+            }
+
+            presentar();
+
+            //Elimino todas las coincidencias
+            while (pila.get(pila.size() - 1).equals(intro.get(0))) {
+                //Verificar si ya terminó
+                if ((pila.get(pila.size() - 1).equals("$")) && (intro.get(0).equals("$"))) {
+                    System.out.println("GRAMATICA TERMINADA [EXITO]");
+                    continuar = false;
+                    break;
+                } else {
+                    coincidencia();
+                }
+
+            }
+        }
+
+        //Presento si hubo errores.
+        if (error) {
+            System.out.println("GRAMATICA CON ERRORES");
+        }
     }
 }
